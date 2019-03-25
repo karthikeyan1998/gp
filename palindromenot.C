8245f0b5806e442071c9efbd7bfe601ea5f97d50
karthikeyan1998 <47741331+karthikeyan1998@users.noreply.github.com>
2019-03-25 16:06:40 +0530

    Create palindromenot.C

---
+++ palindromenot.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,pal,i,a,temp=0;
+scanf("%d",&n);
+pal=n;
+for(i=0;pal!=0;i++)
+{
+    a=pal%10;
+    temp=temp*10 + a;
+    pal=pal/10;
+}
+if(n==temp)
+    printf("palindrome");
+return 0;
+}
